aeee5d793d5eff0e2b1925c7b37b4b436fbc521f
Pranjal Singh <pranjal.dalu@gmail.com>
2021-11-13 23:26:33 +0530

    search bar added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap'
+import { InputGroup, Button } from 'react-bootstrap';
+import './header.css'
 import Travel from '../../assets/travel.png'
 
 function Header() {
     return (
-        <Navbar className='py-0' bg="dark" variant="dark">
-            <Navbar.Brand>
+        <div className='navigation'>
+            <div className='heading'>
                 <img
                     src={Travel}
                     width="30"
                     height="30"
-                    className="d-inline-block"
+                    className="d-inline-block ml-2 align-top"
                     alt="Travel logo"
                 />
-                <h3 className='d-inline-block ml-2 align-center'>Travel Advisor</h3>
-            </Navbar.Brand>
-            <Nav className="ml-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#features">Features</Nav.Link>
-                <Nav.Link href="#pricing">Pricing</Nav.Link>
-            </Nav>
-        </Navbar>
+            <h3 className='d-inline-block ml-2'>Travel Advisor</h3>
+            </div>
+            <InputGroup className='search-bar'>
+                <span className="input-group-prepend">
+                    <div className="input-group-text bg-transparent border-right-0"><i className="fa fa-search"></i></div>
+                </span>
+                <input className="form-control py-2 border-left-0 border" type="search" placeholder="Search places.." />
+                <span className="input-group-append">
+                    <Button className="border-left-0 border" style={{ backgroundColor: '#D5BFBF', color: '#316B83' }} type="submit">
+                        Search
+                    </Button>
+                </span>
+            </InputGroup> 
+        </div>
     )
 }
 
